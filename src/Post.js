47fcd0fc12f2fc99c4d9e4eb84ac7cfc9529e8c8
Lillian Chernin <l.smith1654@gmail.com>
2018-01-29 14:41:02 -0600

    create edit blog post functionality

--- src/Post.js
+++ src/Post.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './Post.css';
+import Comment from './Comment.js'
 
 class Post extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      title: "My initial blog title",
+      author: "My initial blog author",
+      body: "My initial blog post body",
+      comments: ["My first comments at first", "My second comment at first", "My third comment at first"]
+    }
+  }
+  handleClick (e) {
+    this.setState({
+      body: prompt("update blog post", this.state.body)
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="Post">
+        <header className="Post-header">
+          <h1 className="Post-title">{this.state.title}</h1>
+          <h4>Written by {this.state.author}</h4>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <article className="Post-body">
+          <p>{this.state.body}
+          </p>
+          <h3>Comments:</h3>
+          {this.state.comments.map((comment) => {
+            return <Comment body={comment} />
+          })}
+        </article>
+        <button onClick={(e) => this.handleClick(e)}>Update Post?</button>
       </div>
-    );
+    )
   }
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <h1 className="App-title">Welcome to React</h1>
+  //       </header>
+  //       <p className="App-intro">
+  //         To get started, edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //     </div>
+  //   );
+  // }
 }
 
 export default Post;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import Post from './Post';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Post />, document.getElementById('root'));
+ReactDOM.render(<Post title={"My Blog Title"} author={"My Blog Author"} body={"My fascinating blog post body"} comments={["my first comment","my second comment","my third comment"]}/>, document.getElementById('root'));
 registerServiceWorker();
